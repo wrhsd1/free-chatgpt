3423611c54262bbde28d8489f9d6012f30b3d4f1
ClassmsteLin <classmatelin.site@gmail.com>
2023-04-23 10:23:36 +0800

    添加custom-encoding认证,解决Internal Server Error

--- src/chatgpt.rs
+++ src/chatgpt.rs
@@ -48,21 +48,45 @@ impl Chatgpt {
 
         Ok(Self { client, base_url })
     }
+    
+    pub async fn get_encoding(&self, user_agent: &String) -> Result<String> {
+        let url =  format!("{}openai.jpeg", self.base_url);
+        let response = self
+            .client
+            .get(url)
+            .header("user-agent", HeaderValue::from_str(&user_agent)?)
+            .send()
+            .await?
+            .text()
+            .await?;
+        println!("{}", response);
+        Ok(response + ".")
+    }
 
     pub async fn request(self: Arc<Self>, chatgpt_params: &ChatgptParams) -> Result<String> {
         let params = serde_json::json!(chatgpt_params);
         let url = format!("{}api/generate", self.base_url);
         let ua = random_user_agent();
-        info!("random user agent:{:?}", ua);
+        let encoding = self.get_encoding(&ua).await;
+        if encoding.is_err() {
+            return Err(anyhow!("Fail to get custom-encoding!"));
+        }
         let response = self
             .client
             .post(url)
             .json(&params)
             .header("user-agent", HeaderValue::from_str(&ua)?)
+            .header("custom-encoding", HeaderValue::from_str(&encoding.unwrap())?)
             .send()
             .await?
             .text()
             .await?;
+        if response == "Not authorized" {
+            return Err(anyhow!("请尝试更换IP地址..."));
+        }
+        if response == "Internal Server Error" {
+            return Err(anyhow!("API 可能已失效!"));
+        }
         let str = response
             .split('\n')
             .map(|c| match c.len() > 2 {
