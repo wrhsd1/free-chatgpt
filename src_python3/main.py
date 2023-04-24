e868a12f9a145e9583230b0da36680d21924b29d
mingyue0094 <shmily_006@qq.com>
2023-04-24 15:29:17 +0800

    Create main.py

---
+++ src_python3/main.py
@@ -0,0 +1,26 @@
+from chatgpt import GPT_VERCEL as Chatgpt
+from flask import Flask, request,  jsonify,stream_with_context,Response
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def __index():
+    return "hello"
+
+
+@app.route('/api', methods=['GET', 'POST'])
+def _openai():
+    
+    messages = request.json.get("messages",'')
+    model = request.json.get("model","openai:gpt-3.5-turbo")
+    txt = request.json.get("prompt","")
+
+    chatgpt = Chatgpt()
+    res = chatgpt.ask(txt,model)
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port=8000)
+    
